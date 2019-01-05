e35119ffbfcd3217091fba047617bd954f5828a0
yetbetter <costilek@gmail.com>
2019-01-05 15:57:20 +0300

    For push

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from config import ProductionConfig
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config.from_object(ProductionConfig)
 
 @app.route('/')
 def hello():
-    return 'Hello'
+    mail = Mail(app)
+    msg = Message(
+    	'Hello', 
+    	sender='kaduk9393@gmail.com',
+    	recipients=['costilek@gmail.com']
+    )
+    msg.body = 'testing'
+    msg.html = '<b>testing</b>'
+    return mail.send(msg)
