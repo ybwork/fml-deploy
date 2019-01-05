6a695a7d458cdbf142c36512a333ba6476d11559
yetbetter <costilek@gmail.com>
2019-01-05 17:03:08 +0300

    For push

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
 from flask import current_app as app
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Length
